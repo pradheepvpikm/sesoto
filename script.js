eb5a65034aeefaacaca6581de1ff252c8534fc14
pradheepvpikm <pradheepvpikm@gmail.com>
2024-08-20 12:15:58 +0530

    cart cript updated

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-var img=document.createElement(Image)
-function imgClick(){
-  img.src="images/3 way (599).jpg"
-  document.body.
+const parantDiv = document.getElementById('test-container');
+
+document.getElementById('cart').style.display='none';
+
+// const btnText=document.getElementById('add-div')
+function hideCart(){
+  document.getElementById('cart').style.display='none';
 }
+function addText(){
+  const div1=document.createElement('div');
+  div1.setAttribute('id','cart-div1');
+  div1.style.border('1px')
+  div1.style.backgroundColor('red')
+  parantDiv.appendChild(div1);
+  
+   
+
+
+
+}
+function hide(){
+  const btntext= document.getElementById('cart');
+if(btntext.style.display!='none'){
+  // document.getElementById('add-text')
+ btntext.style.display='none';
+}else{
+  btntext.style.display='block';
+
+}
+// btntext.style.display='none'
+}
+
+  
